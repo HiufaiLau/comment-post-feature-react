ed35a38b30c26efcf0dfdcfb494d6d245e18ef30
Hiul <hiufailau.41@gmail.com>
2023-08-01 19:26:29 +0200

    added thunk middleware for  asynchronous

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,39 +1,94 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "./postSlice";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "./postSlice";
+import { useEffect } from "react";
+import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
+  const dispatch = useDispatch();
+
   const posts = useSelector(selectAllPosts);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
 
-  // added a sort function to sort the posts by date
-  // use slice to create an array copy
-  const orderedPosts = posts
-    .slice()
-    .sort((a, b) => b.date.localeCompare(a.date));
+  useEffect(() => {
+    if (postStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postStatus, dispatch]);
 
-  const renderedPosts = orderedPosts.map((post) => (
-    <article key={post.id}>
-      <h3>{post.title}</h3>
-      {/* using substring to limit the length of the content */}
-      <p>{post.content.substring(0, 100)}</p>
-      <p className='postCredit'>
-        {/* this is a prop to pass the userId to PostAuthor component */}
-        <PostAuthor userId={post.userId} />
-        <TimeAgo timestamp={post.date} />
-      </p>
-
-      <ReactionButtons post={post} />
-    </article>
-  ));
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
 
   return (
     <section>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
-
 export default PostsList;
+
+// import { useSelector, useDispatch } from "react-redux";
+// import {
+//   selectAllPosts,
+//   getPostStatus,
+//   getPostError,
+//   fetchPosts,
+// } from "./postSlice";
+// import { useEffect } from "react";
+// import PostExcerpt from "./PostExcerpt";
+
+// const PostsList = () => {
+//   const dispatch = useDispatch();
+
+//   const posts = useSelector(selectAllPosts);
+//   const postStatus = useSelector(getPostStatus);
+//   const error = useSelector(getPostError);
+//   console.log("posts", posts);
+
+//   useEffect(() => {
+//     if (postStatus === "idle") {
+//       dispatch(fetchPosts());
+//     }
+//   }, [postStatus, dispatch]);
+
+//   // added a sort function to sort the posts by date
+//   // use slice to create an array copy of the posts
+//   let content;
+//   if (postStatus === "loading") {
+//     content = <p>"Loading..."</p>;
+//   } else if (postStatus === "succeeded") {
+//     const orderedPosts = posts
+//       .slice()
+//       .sort((a, b) => b.date.localeCompare(a.date));
+//     content = orderedPosts.map((post) => (
+//       <PostExcerpt key={post.id} post={post} />
+//     ));
+//   } else if (postStatus === "failed") {
+//     content = <p>{error}</p>;
+//   }
+
+//   return (
+//     <section>
+//       <h2>Posts</h2>
+//       {content}
+//     </section>
+//   );
+// };
+// export default PostsList;
